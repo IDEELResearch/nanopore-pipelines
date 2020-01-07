e4a166b6ed4dbc25d32cb0fd616005583e2a3fac
Kara Moser <moserk9@gmail.com>
2020-01-07 14:59:42 -0500

    begin option loop

--- alignment/do-alignment.sh
+++ alignment/do-alignment.sh
@@ -10,20 +10,34 @@ set -o xtrace
 # kamoser
 # Jan 2020
 
-#Required software
-#samtools, ngmlr
-#example:
-#ngmlr=/nas/longleaf/home/kamoser/bin/ngmlr-0.2.7/ngmlr
-
 # ABOUT
 
 # This pipeline takes zipped fastq files, aligns them to a reference with the 
 # long-read aligner nglmr, and then generates summary statistics
 
+# run as ./script <alignment option> (see below)
+
 # TODO
 # make updates so you can choose ngmlr or minimap (or both?)
 
 ###############################################################################
+
+# To supply on the command line: 
+# Would you like alignment using:
+#   "minimap"
+#   "ngmlr"
+#   "both"
+
+option=$1
+
+###############################################################################
+
+# Paths, env
+
+#Required software
+#samtools, ngmlr
+#example:
+#ngmlr=/nas/longleaf/home/kamoser/bin/ngmlr-0.2.7/ngmlr
 
 #change as needed
 data_d=<path_to_zipped_fastq>
@@ -35,19 +49,42 @@ cd $work_d
 
 ###############################################################################
 
-b=sort.bam
-if [[ ! -e $b ]]; then
+if [[ option == "minimap" ]]; then
+  
+  if [[ ]]; then 
 
-  $ngmlr -t 10 -r $ref -q $data_d/*fastq.gz -o test.sam
+    minimap2 -ax map-ont $ref $work_d/*fastq
+    samtools view -S -b test.sam > ngmlr.bam
+    samtools sort ngmlr.bam -o $b
+    samtools index $b
 
-  samtools view -S -b test.sam > test.bam
-  samtools sort test.bam -o $b
-  samtools index $b
+    rm test.sam
+    rm test.bam
 
-  rm test.sam
-  rm test.bam
+  fi
+
+else if [[ $option == "ngmlr" ]]; then
+
+  b=ngmlr.sort.bam
+  if [[ ! -e $b ]]; then
+
+    $ngmlr -t 10 -r $ref -q $data_d/*fastq.gz -o test.sam
+
+    samtools view -S -b test.sam > ngmlr.bam
+    samtools sort ngmlr.bam -o $b
+    samtools index $b
+
+    rm test.sam
+    rm test.bam
+
+  fi
+
+else if [[ option == "both" ]]; then
+
+
 
 fi
+
 
 f=alignment.flagstat
 if [[ ! -e $f ]]; then

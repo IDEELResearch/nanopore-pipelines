3bbe403a5efcd169ff89ca06af527b473920b045
Kara Moser <moserk9@gmail.com>
2020-02-18 16:00:48 -0500

    fix reference

--- alignment/do-alignment-ngmlr.py
+++ alignment/do-alignment-ngmlr.py
@@ -12,17 +12,17 @@
 ## CHANGE AS NEEDED
 
 ## Reference genome
-REF = /proj/ideel/resources/genomes/Tpallidum/ 
+REF = '/proj/ideel/resources/genomes/Tpallidum/Nichols_NC_021490.2.fasta'
 
 ## Required software
 # samtools should be in your path (module load samtools)
 ngmlr = '/nas/longleaf/home/kamoser/bin/ngmlr-0.2.7/ngmlr'
 
 ## Project specfic paths
-workdir: '/pine/k/a/kamoser/nanopore-tpallidium/test-alignments/ngmlr' #sets working dir; most likely scratch
-WRKDIR = '/pine/k/a/kamoser/nanopore-tpallidium/test-alignments/ngmlr' #setting string var
+workdir: '/pine/scr/k/a/kamoser/nanopore-tpallidium/test-alignments/ngmlr' #sets working dir; most likely scratch
+WRKDIR = '/pine/scr/k/a/kamoser/nanopore-tpallidium/test-alignments/ngmlr' #setting string var
 
-readWD = '/pine/scr/k/a/kamoser/nanopore-tpallidium/test-alignments' # where are your reads kept?
+readWD = '/pine/scr/k/a/kamoser/nanopore-tpallidium/test-alignments/symlinks/' # where are your reads kept?
 
 SAMPLES, = glob_wildcards(readWD + '{samp}.fastq') #getting samples
 
@@ -37,17 +37,17 @@ rule all:
 # align reads to reference 
 rule align:
 	input:
-		"{samp}.fastq"
+		readWD + "{samp}.fastq"
 	output:
 		"{samp}.sam"
 	shell: 
-		"ngmlr -t 4 -x ont --bam-fix \
+		"{ngmlr} -t 4 -x ont --bam-fix \
 		-r {REF} \
 		-q {input} \
 		-o {output}"
 
 #convert sam to bam
-rule convert-sam:
+rule convertsam:
 	input:
 		"{samp}.sam"
 	output:
@@ -57,10 +57,13 @@ rule convert-sam:
 			> {output}"
 
 #sort and index:
+rule sort:
 	input:
 		"{samp}.bam"
 	output:
-		"{samp}.sorted.bam.bai"
+		sorted = "{samp}.sorted.bam",
+		index = "{samp}.sorted.bam.bai"
 	run:
-		shell("samtools sort {input} -o {samp}.sorted.bam")
-		shell("samtools index {samp}.sorted.bam")
+		shell("samtools sort {input} -o {output.sorted}")
+		shell("samtools index {output.sorted}")
+
